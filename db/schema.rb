55f5595f7ad682cf408f80709c065f3cc55f99e3
RCS <github-actions[bot]@users.noreply.github.com>
2025-07-15 15:21:33 -0400

    Update schema version and add body column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_15_185613) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_15_191500) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -19,5 +19,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_15_185613) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.text "body"
   end
 end
