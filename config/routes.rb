0c8fea4a3c3b9094cf5a182205b49e63ee3a5f72
RCS <github-actions[bot]@users.noreply.github.com>
2025-07-15 16:31:25 -0400

    Add home page with controller, view, and helper; update application layout and routes; enhance post form with status selection

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
