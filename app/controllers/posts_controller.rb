73b39ca4c77769c4f3243e5ced50cb6ab72490aa
RCS <github-actions[bot]@users.noreply.github.com>
2025-07-15 15:25:02 -0400

    Update post_params to require status and body fields

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.expect(post: [ :title, :content, :published, :published_at ])
+      params.require(:post).permit(:title, :content, :published, :published_at, :status, :body)
     end
 end
